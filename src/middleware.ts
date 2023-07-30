8c830857c96c8a88624c6237958b24d486d7f283
iorise <riofirmansyah1805@gmail.com>
2023-07-30 10:49:38 +0700

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,30 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, clerkClient } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/:path*"],
+  publicRoutes: ["/", "/sso-callback(.*)", "/api(.*)"],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      return NextResponse.next();
+    }
+
+    const url = new URL(req.nextUrl.origin);
+
+    if (!auth.userId) {
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
+
+    // Set the user's role to user if it doesn't exist
+    const user = await clerkClient.users.getUser(auth.userId);
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+  },
 });
 
 export const config = {
