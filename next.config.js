11a390df5f33051f164e4f0b432fcd1153d752fe
iorise <riofirmansyah1805@gmail.com>
2023-08-03 17:18:09 +0700

    update package.json and next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.clerk.com", "www.gravatar.com"],
+    domains: ["images.clerk.com", "www.gravatar.com", "lh3.googleusercontent.com"],
   },
 };
 
