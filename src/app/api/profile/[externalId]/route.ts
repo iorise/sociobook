b650020896fc1834b08b5ee0c7e1b998b6b9fc73
iorise <riofirmansyah1805@gmail.com>
2023-08-09 09:30:26 +0700

    add user api route

---
+++ src/app/api/profile/[externalId]/route.ts
@@ -0,0 +1,62 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { externalId: string } }
+) {
+  const { userId } = auth();
+
+  if (!userId) {
+    return new NextResponse("No user found", { status: 400 });
+  }
+  try {
+    if (!params.externalId) {
+      return new NextResponse("No user found", { status: 400 });
+    }
+
+    const user = await prismadb.user.findUnique({
+      where: {
+        externalId: params.externalId,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log("[USER_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { externalId: string } }
+) {
+  try {
+    const body = await request.json();
+
+    const { firstName, lastName, externalImage, coverImage } = body;
+
+    if (!params.externalId) {
+      return new NextResponse("No user found", { status: 400 });
+    }
+
+    const user = await prismadb.user.update({
+      where: {
+        externalId: params.externalId,
+      },
+      data: {
+        firstName,
+        lastName,
+        externalImage,
+        coverImage,
+      },
+    });
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log("[USER_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
---
+++ src/app/api/profile/route.ts
@@ -0,0 +1,25 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const user = await prismadb.user.findMany({
+      where: {
+        externalId: {
+          not: userId
+        }
+      }
+    })
+
+    return NextResponse.json(user);
+  } catch (error) {
+    console.log("[USER_GET]", error);
+    return new NextResponse("Internal error", {status: 500});
+  }
+}
