c35e02c12e8d659efe07c608685c7dbfc4f7940e
iorise <riofirmansyah1805@gmail.com>
2023-08-10 10:36:58 +0700

    fix error

--- src/hooks/use-posts.ts
+++ src/hooks/use-posts.ts
@@ -1,4 +1,17 @@
+import fetcher from "@/lib/fetcher";
 import axios from "axios";
+import useSWR from "swr";
+
+
+export function usePosts  (externalId?: string) {
+  const url = externalId
+    ? `/api/posts/${externalId}`
+    : `/api/posts`;
+
+  const { data, error, isLoading, mutate } = useSWR(url, fetcher);
+
+  return { data, error, isLoading, mutate };
+};
 
 type UserQueryParams = {
   externalId?: string | null
