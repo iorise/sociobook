3f19eb3579bdbf7f7dbd95333ddfeac0ee6112ee
iorise <riofirmansyah1805@gmail.com>
2023-09-28 13:40:19 +0700

    change bg overlay from blur to black/80

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react";
 import Link from "next/link";
-import Image from "next/image";
 import { User } from "@prisma/client";
+import { motion, AnimatePresence } from "framer-motion";
 
 import {
   Card,
@@ -24,6 +24,7 @@ import { UserName } from "@/components/ui/user-name";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { usePost } from "@/hooks/use-post";
 import { ImagePreview } from "@/components/image-preview";
+import { setTransition } from "@/lib/transition";
 
 interface PostProps {
   data: extendedPost;
@@ -58,169 +59,178 @@ export function Post({ data, currentUser }: PostProps) {
   }, [deletePost]);
 
   return (
-    <div className="flex flex-col gap-5">
-      <Card>
-        {/* <Link href={`/post/${data.id}`}> */}
-          <CardHeader className="space-y-1 p-4">
-            <div className="flex justify-between">
-              <Link href={`/profile/${data.user?.externalId}`}>
-                <div className="flex gap-2 items-center">
-                  <Avatar>
-                    <AvatarImage
-                      src={
-                        data.user?.externalImage ?? data.user.profileImage ?? ""
-                      }
-                      alt={data.user?.firstName ?? ""}
-                    />
-                    <AvatarFallback>
-                      <img src="/images/placeholder.png" />
-                    </AvatarFallback>
-                  </Avatar>
-                  <div className="flex flex-col px-[-1rem]">
-                    <UserName
-                      firstName={data.user?.firstName}
-                      lastName={data.user?.lastName}
-                      verified={data.user?.verified}
-                      className="text-lg font-medium"
-                    />
-                    <span className="font-normal text-muted-foreground text-xs">
-                      {createdAt}
-                    </span>
+    // <AnimatePresence>
+    //   <motion.div layout {...setTransition()}>
+        <div className="flex flex-col gap-5">
+          <Card className="rounded-sm">
+            {/* <Link href={`/post/${data.id}`}> */}
+            <CardHeader className="space-y-1 p-4">
+              <div className="flex justify-between">
+                <Link href={`/profile/${data.user?.externalId}`}>
+                  <div className="flex gap-2 items-center">
+                    <Avatar>
+                      <AvatarImage
+                        src={
+                          data.user?.externalImage ??
+                          data.user.profileImage ??
+                          ""
+                        }
+                        alt={data.user?.firstName ?? ""}
+                      />
+                      <AvatarFallback>
+                        <img src="/images/placeholder.png" />
+                      </AvatarFallback>
+                    </Avatar>
+                    <div className="flex flex-col px-[-1rem]">
+                      <UserName
+                        firstName={data.user?.firstName}
+                        lastName={data.user?.lastName}
+                        verified={data.user?.verified}
+                        className="text-lg font-medium"
+                      />
+                      <span className="font-normal text-muted-foreground text-xs">
+                        {createdAt}
+                      </span>
+                    </div>
                   </div>
-                </div>
-              </Link>
-              {postByCurrentUser && (
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <Button variant="ghost" className="rounded-full">
-                      <Icons.moreHorizontal className="w-4 h-4" />
-                    </Button>
-                  </PopoverTrigger>
-                  <PopoverContent
-                    className="w-40 bg-background p-2"
-                    align="end"
-                    sideOffset={2}
-                  >
-                    <Button
-                      variant="ghost"
-                      size="icon"
-                      onClick={onDelete}
-                      disabled={isDeletingPost}
-                      className="rounded-md w-full"
+                </Link>
+                {postByCurrentUser && (
+                  <Popover>
+                    <PopoverTrigger asChild>
+                      <Button variant="ghost" className="rounded-full">
+                        <Icons.moreHorizontal className="w-4 h-4" />
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent
+                      className="w-40 bg-background p-2"
+                      align="end"
+                      sideOffset={2}
                     >
-                      {isDeletingPost ? (
-                        <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
-                      ) : (
-                        <Icons.trash className="w-4 h-4 mr-2" />
-                      )}
-                      <span className="font-medium text-sm">Delete</span>
-                    </Button>
-                  </PopoverContent>
-                </Popover>
-              )}
-            </div>
-          </CardHeader>
-        {/* </Link> */}
-        <CardContent>
-          <div className="w-full flex flex-col">
-            <p
-              onClick={toggleShowMoreText}
-              className={cn(
-                "break-words",
-                showMoreText
-                  ? "line-clamp-none"
-                  : data.images.length
-                  ? `line-clamp-3`
-                  : "line-clamp-6"
-              )}
-            >
-              {data.text}
-            </p>
-            <div
-              className={cn("w-full grid",
-                data.images.length > 1
-                  ? "grid-cols-2 gap-1 relative rounded-md"
-                  : "grid-cols-1"
-              )}
-            >
-              {data.images.map((image, i) => (
-                <ImagePreview
-                  key={i}
-                  imageData={image}
-                  i={i}
-                  imagesLength={data.images.length}
-                  imagesPreview={data.images}
-                />
-              ))}
-            </div>
-          </div>
-        </CardContent>
-        <CardFooter className="flex flex-col text-muted-foreground">
-          <div className="flex w-full justify-between text-muted-foreground text-sm mb-1">
-            {likeCount > 0 && (
-              <div className="flex items-center gap-1">
-                <Icons.thumbFill className="text-facebook-primary w-4 h-4" />
-                <span>{likeCount}</span>
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        onClick={onDelete}
+                        disabled={isDeletingPost}
+                        className="rounded-md w-full"
+                      >
+                        {isDeletingPost ? (
+                          <Icons.spinner className="w-4 h-4 mr-2 animate-spin" />
+                        ) : (
+                          <Icons.trash className="w-4 h-4 mr-2" />
+                        )}
+                        <span className="font-medium text-sm">Delete</span>
+                      </Button>
+                    </PopoverContent>
+                  </Popover>
+                )}
               </div>
-            )}
-
-            {data.comments.length !== 0 ? (
-              <div className="flex w-full justify-end">
-                <div>
-                  <span className="flex items-center gap-1">
-                    <p>{data.comments.length}</p>
-                    <Icons.comment className="w-4 h-4 md:hidden" />
-                    <p className="hidden md:block">Comments</p>
-                  </span>
+            </CardHeader>
+            {/* </Link> */}
+            <CardContent>
+              <div className="w-full flex flex-col">
+                <p
+                  onClick={toggleShowMoreText}
+                  className={cn(
+                    "break-words",
+                    showMoreText
+                      ? "line-clamp-none"
+                      : data.images.length
+                      ? `line-clamp-3`
+                      : "line-clamp-6"
+                  )}
+                >
+                  {data.text}
+                </p>
+                <div
+                  className={cn(
+                    "w-full grid",
+                    data.images.length > 1
+                      ? "grid-cols-2 gap-1 relative rounded-md"
+                      : "grid-cols-1"
+                  )}
+                >
+                  {data.images.map((image, i) => (
+                    <ImagePreview
+                      key={i}
+                      imageData={image}
+                      i={i}
+                      imagesLength={data.images.length}
+                      imagesPreview={data.images}
+                    />
+                  ))}
                 </div>
               </div>
-            ) : null}
-          </div>
-          <Separator className="mb-2" />
-          <div className="flex w-full text-base font-medium">
-            <Button
-              variant="ghost"
-              className="flex-1 active:scale-95"
-              onClick={() => toggleLike()}
-            >
-              <span
-                className={cn(
-                  "flex items-center",
-                  hasLiked ? "text-facebook-primary" : "text-muted-foreground"
+            </CardContent>
+            <CardFooter className="flex flex-col text-muted-foreground">
+              <div className="flex w-full justify-between text-muted-foreground text-sm mb-1">
+                {likeCount > 0 && (
+                  <div className="flex items-center gap-1">
+                    <Icons.thumbFill className="text-facebook-primary w-4 h-4" />
+                    <span>{likeCount}</span>
+                  </div>
                 )}
-              >
-                <Icons.thumb className={cn("w-6 h-6 mr-2")} />
-                <span>Like</span>
-              </span>
-            </Button>
-            <Button
-              variant="ghost"
-              className="flex-1 active:scale-95"
-              onClick={toggleComment}
-            >
-              <Icons.comment className="w-6 h-6 mr-2" />
-              <span>Comment</span>
-            </Button>
-            <Button
-              variant="ghost"
-              className="flex-1 sm:flex cursor-not-allowed"
-              disabled
-            >
-              <Icons.share className="w-6 h-6 mr-2" />
-              <span>Share</span>
-            </Button>
-          </div>
-          {showComment && (
-            <>
-              <Separator className="mt-2" />
-              <div className="w-full flex flex-col pt-2">
-                <CommentList postId={postId} />
-                <CommentForm currentUser={currentUser} postId={postId} />
+
+                {data.comments.length !== 0 ? (
+                  <div className="flex w-full justify-end">
+                    <div>
+                      <span className="flex items-center gap-1">
+                        <p>{data.comments.length}</p>
+                        <Icons.comment className="w-4 h-4 md:hidden" />
+                        <p className="hidden md:block">Comments</p>
+                      </span>
+                    </div>
+                  </div>
+                ) : null}
               </div>
-            </>
-          )}
-        </CardFooter>
-      </Card>
-    </div>
+              <Separator className="mb-2" />
+              <div className="flex w-full text-base font-medium">
+                <Button
+                  variant="ghost"
+                  className="flex-1 active:scale-95"
+                  onClick={() => toggleLike()}
+                >
+                  <span
+                    className={cn(
+                      "flex items-center",
+                      hasLiked
+                        ? "text-facebook-primary"
+                        : "text-muted-foreground"
+                    )}
+                  >
+                    <Icons.thumb className={cn("w-6 h-6 mr-2")} />
+                    <span>Like</span>
+                  </span>
+                </Button>
+                <Button
+                  variant="ghost"
+                  className="flex-1 active:scale-95"
+                  onClick={toggleComment}
+                >
+                  <Icons.comment className="w-6 h-6 mr-2" />
+                  <span>Comment</span>
+                </Button>
+                <Button
+                  variant="ghost"
+                  className="flex-1 sm:flex cursor-not-allowed"
+                  disabled
+                >
+                  <Icons.share className="w-6 h-6 mr-2" />
+                  <span>Share</span>
+                </Button>
+              </div>
+              {showComment && (
+                <>
+                  <Separator className="mt-2" />
+                  <div className="w-full flex flex-col pt-2">
+                    <CommentList postId={postId} />
+                    <CommentForm currentUser={currentUser} postId={postId} />
+                  </div>
+                </>
+              )}
+            </CardFooter>
+          </Card>
+        </div>
+    //   {/* </motion.div>
+    // </AnimatePresence> */}
   );
 }
