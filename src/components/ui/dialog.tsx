2e64d6eecc3a2e9c86073f29dd459d96f8a1af6b
iorise <riofirmansyah1805@gmail.com>
2023-10-02 18:50:16 +0700

    delete unused import

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -5,8 +5,6 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
 
 import { cn } from "@/lib/utils";
-import { Button } from "./button";
-import { Icons } from "../icons";
 
 const Dialog = DialogPrimitive.Root;
 
