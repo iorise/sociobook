5f8526115605ed077e6a8b57c71c6a996cc5eb5f
iorise <riofirmansyah1805@gmail.com>
2023-10-03 16:10:27 +0700

    single post fetch

--- src/hooks/use-post.ts
+++ src/hooks/use-post.ts
@@ -1,10 +1,19 @@
-import { useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
-export function usePost(postId: string, queryKey: string) {
+export function usePost(postId: string, queryKey?: string) {
   const queryClient = useQueryClient();
+
+  const { data, isLoading, isError, isSuccess } = useQuery({
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/posts/${postId}`);
+      return data;
+    },
+    queryKey: ["post", postId],
+    refetchOnWindowFocus: false,
+  });
 
   const { mutateAsync: deletePost, isLoading: isDeletingPost } = useMutation({
     mutationFn: async () => {
@@ -18,5 +27,5 @@ export function usePost(postId: string, queryKey: string) {
     },
   });
 
-  return { deletePost, isDeletingPost };
+  return { data, isLoading, isError, isSuccess, deletePost, isDeletingPost };
 }
